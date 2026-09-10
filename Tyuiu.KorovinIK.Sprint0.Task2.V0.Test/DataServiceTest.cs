a3974cf5731dc762ba6573e73ea8f01d6d697a69
Илья Коровин <iliyacoor@gmail.com>
2026-09-10 19:53:56 +0500

    Task2

---
+++ Tyuiu.KorovinIK.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.KorovinIK.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.KorovinIK.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Илья";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Илья", res);
+        }
+    }
+}
+    
---
+++ Tyuiu.KorovinIK.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.KorovinIK.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.KorovinIK.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Илья"));
+            Console.ReadKey();
+        }
+
+    }
+}
