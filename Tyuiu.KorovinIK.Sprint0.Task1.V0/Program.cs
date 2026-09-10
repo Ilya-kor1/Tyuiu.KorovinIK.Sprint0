e050b0fbc2182ec962b26c9f62d7fd0ca1088f6a
Илья Коровин <iliyacoor@gmail.com>
2026-09-10 18:04:06 +0500

    Task1

---
+++ Tyuiu.KorovinIK.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlTypes;
+
+namespace Tyuiu.KorovinIK.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Илья";
+            int age = 18;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadLine();
+
+        }
+
+    }
+}
+
+
