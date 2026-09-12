51d89474a015b4e328a642a24e2227da555c3630
Илья Коровин <iliyacoor@gmail.com>
2026-09-12 11:13:04 +0500

    Task3

---
+++ Tyuiu.KorovinIK.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.KorovinIK.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.KorovinIK.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+
+    }
+}
+
